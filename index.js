a0465b195dc1abdb7c268147f156bb9764cbc521
joshpainter <josh@joshpainter.com>
2022-03-04 03:55:16 -0600

    added eslint and fixed issues

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-const lookup = require('./emojis.json')
-const pluralize = require('pluralize')
+const lookup = require('./emojis.json');
+const pluralize = require('pluralize');
 
 const SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
 
-translations = (original) => {
-  let word = original.trim().toLowerCase()
+const translations = (original) => {
+	let word = original.trim().toLowerCase();
 
-  if (lookup[word]) return lookup[word]
+	if (lookup[word]) return lookup[word];
 
-  // Attempt to pluaralize.
-  if (pluralize.isPlural(word)) {
-    word = pluralize.singular(word)
-    if (lookup[word]) return lookup[word]
-  }
+	// Attempt to pluaralize.
+	if (pluralize.isPlural(word)) {
+		word = pluralize.singular(word);
+		if (lookup[word]) return lookup[word];
+	}
 
-  // Attempt to un-conjugate verb.
-  if (word.indexOf('ing') !== -1) {
-    let verb = word.substr(0, word.length - 3);
-    // eating -> eat
-    if (lookup[verb]) return lookup[verb]
-    // dancing -> dance
-    if (lookup[verb + 'e']) return lookup[verb + 'e']
-    // running -> run
-    let verbDoubled = verb.substr(0, verb.length - 1);
-    if (lookup[verbDoubled]) return lookup[verbDoubled]
-  }
-}
+	// Attempt to un-conjugate verb.
+	if (word.indexOf('ing') !== -1) {
+		const verb = word.substr(0, word.length - 3);
+		// eating -> eat
+		if (lookup[verb]) return lookup[verb];
+		// dancing -> dance
+		if (lookup[verb + 'e']) return lookup[verb + 'e'];
+		// running -> run
+		const verbDoubled = verb.substr(0, verb.length - 1);
+		if (lookup[verbDoubled]) return lookup[verbDoubled];
+	}
+};
 
-translateWord = (original) => {
-  options = translations(original)
-  if (options === undefined || options.length === 0)
-    return
-  return options[Math.floor(Math.random() * options.length)]
-}
+const translateWord = (original) => {
+	const options = translations(original);
+	if (options === undefined || options.length === 0) {return;}
+	return options[Math.floor(Math.random() * options.length)];
+};
 
-translate = (text) => {
-  let translation = ''
-  let words = text.split(' ')
-  for (let word of words) {
-    // Get rid of punctuation at the start and end of word.
-    let firstSymbol = ''
-    let lastSymbol = ''
+const translate = (text) => {
+	let translation = '';
+	const words = text.split(' ');
+	for (let word of words) {
+		// Get rid of punctuation at the start and end of word.
+		let firstSymbol = '';
+		let lastSymbol = '';
 
-    while (SYMBOLS.indexOf(word[0]) != -1) {
-      firstSymbol += word[0]
-      word = word.slice(1, word.length)
-    }
-    while (SYMBOLS.indexOf(word[word.length - 1]) != -1) {
-      lastSymbol += word[word.length - 1]
-      word = word.slice(0, word.length - 1)
-    }
+		while (SYMBOLS.indexOf(word[0]) != -1) {
+			firstSymbol += word[0];
+			word = word.slice(1, word.length);
+		}
+		while (SYMBOLS.indexOf(word[word.length - 1]) != -1) {
+			lastSymbol += word[word.length - 1];
+			word = word.slice(0, word.length - 1);
+		}
 
-    let translated = translateWord(word);
-    if (translated) {
-      translation += firstSymbol + translated + lastSymbol + ' '
-    } else {
-      translation += firstSymbol + word + lastSymbol +  ' '
-    }
-  }
-  return translation
-}
+		const translated = translateWord(word);
+		if (translated) {
+			translation += firstSymbol + translated + lastSymbol + ' ';
+		}
+		else {
+			translation += firstSymbol + word + lastSymbol + ' ';
+		}
+	}
+	return translation;
+};
 
 module.exports = {
-  translate: translate
-}
+	translate: translate,
+};
--- inverter.js
+++ inverter.js
@@ -1,31 +1,31 @@
-const emojilib = require('emojilib')
-const fsPromises = require('fs').promises
+const emojilib = require('emojilib');
+const fsPromises = require('fs').promises;
 
-invert = () => {
-  let inverted = {}
-  for (let [emoji, keywords] of Object.entries(emojilib)) {
-    for (const keyword of keywords) {
-      emojiArray = inverted[keyword] ?? (inverted[keyword] = new Array());
-      if (!emojiArray.includes(emoji)) emojiArray.push(emoji);
-    }
-  }
-  return inverted
-}
+const invert = () => {
+	const inverted = {};
+	for (const [emoji, keywords] of Object.entries(emojilib)) {
+		for (const keyword of keywords) {
+			const emojiArray = inverted[keyword] ?? (inverted[keyword] = new Array());
+			if (!emojiArray.includes(emoji)) emojiArray.push(emoji);
+		}
+	}
+	return inverted;
+};
 
-invertToFile = async (path) => {
-  let handle
-  try {
-    handle = await fsPromises.open(path, 'w')
-    const inverted = invert()
-    await handle.writeFile(JSON.stringify(inverted))
-  } finally {
-    if (handle != undefined)
-      await handle.close()
-  }
-}
+const invertToFile = async (path) => {
+	let handle;
+	try {
+		handle = await fsPromises.open(path, 'w');
+		const inverted = invert();
+		await handle.writeFile(JSON.stringify(inverted));
+	}
+	finally {
+		if (handle != undefined) {await handle.close();}
+	}
+};
 
 const start = async () => {
-  await invertToFile('./emojis.json');
-}
+	await invertToFile('./emojis.json');
+};
 
 start();
