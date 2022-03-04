1f9cf8ae85c296997f81dbdfcb62bf10496d16ae
joshpainter <josh@joshpainter.com>
2022-03-04 03:16:28 -0600

    optimize/update for 3.0.x

--- inverter.js
+++ inverter.js
@@ -7,11 +7,8 @@ invert = () => {
   let inverted = {}
   for (let [emoji, keywords] of Object.entries(emojilib)) {
     for (const keyword of keywords) {
-      if (inverted[keyword]) {
-        inverted[keyword].push(emoji)
-      } else {
-        inverted[keyword] = [emoji]
-      }
+      emojiArray = inverted[keyword] ?? (inverted[keyword] = new Array());
+      if (!emojiArray.includes(emoji)) emojiArray.push(emoji);
     }
   }
   return inverted
